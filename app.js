e01390ee9759440c11d6507dbdcfde0076fc39cc
vijayvlece <vijayvl632@gmail.com>
2025-04-06 13:13:27 +0530

    Initinal commit:second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/",(req,res) => {
-    res.send("Welcome to ShopEasy!");
+    res.send("Welcome to ShopEasy from jaisriram construction!");
 })
 
 app.listen(PORT,() =>{
